c65b27aa6fdbc8153bc53e50cf04d47f790dace5
Gyanendra Mishra <anomaly.the@gmail.com>
2016-02-19 21:15:02 +0530

    added upvote downvote to backend

--- Core/api.py
+++ Core/api.py
@@ -8,7 +8,7 @@ app = FlaskAPI(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client.revmine
 
-def createve_rev(domain,pid,product_name=None):
+def createve_rev(domain,pid,product_name=None, upvotes, downvotes):
     try:
     	print 'doesnt exist'
         justDoIt(domain,pid,product_name)
@@ -17,7 +17,7 @@ def createve_rev(domain,pid,product_name=None):
         if foo:
             return foo
         else:
-            return {'status':69, 'result':110, 'reviews':['Not Applicable'], 'valid':0}
+            return {'status':69, 'result':110, 'reviews':['Not Applicable'], 'valid':0, 'upvotes': upvotes, 'downvotes': downvotes}
     except:
         return {'status':69, 'result':100, 'reviews':['Not Applicable'], 'valid':0}
 
@@ -27,16 +27,47 @@ def getRatings(domain,pid,product_name=None):
     Retrieve, update or delete note instances.
     """
     foo = db.result.find_one({'_id' : pid, 'domain' :domain, 'valid':1 })
+
+    upvotes = 0
+    downvotes= 0
+
+    if db.votes.count({'_id': pid, 'domain': domain}) == 0:
+        db.votes.insert({'_id': pid, 'domain': domain, 'upvotes': 0, 'downvotes': 0})
+        upvotes = 0
+        downvotes = 0
+    else:
+        upvotes = db.votes.find_one({'_id': pid, 'domain': domain})['upvotes']
+        downvotes = db.votes.find_one({'_id': pid, 'domain': domain})['downvotes']
+
     if not foo:
         try:
-            foo = createve_rev(domain,pid,product_name)
+            foo = createve_rev(domain,pid,product_name, upvotes, downvotes)
         except:
             return {'status':69, 'result':100, 'reviews':['Not Applicable']}
 
     if foo and foo.has_key('topics'):
-        return {'result': foo['topics'], 'reviews':foo['sentences'], 'status':200}
+        return {'result': foo['topics'], 'reviews':foo['sentences'], 'status':200, 'upvotes': upvotes, 'downvotes': downvotes}
     else:
         return {'status': 100, 'reviews':['Not Applicable'], 'result': 100}
+
+
+@app.route("/vote/<vote>/<domain>/<pid>/", methods=['GET'])
+def getRatings(vote, domain,pid):
+    """
+    Retrieve, update or delete note instances.
+    """
+    try:
+        if db.votes.count({'_id': pid, 'domain': domain}) == 0:
+            db.votes.insert({'_id': pid, 'domain': domain, 'upvotes': 0, 'downvotes': 0})
+        if vote =='up':
+            db.votes.update({'_id': pid, 'domain': domain}, {'$inc': {'upvotes' : 1}})
+        else:
+            db.votes.update({'_id': pid, 'domain': domain}, {'$inc': {'downvotes' : 1}})
+
+    except:
+        return {'status': 100, 'reviews':['Not Applicable'], 'result': 100}
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
